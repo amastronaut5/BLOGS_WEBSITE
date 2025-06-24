fed96222be2ac0cf6bcb98fc5af5e8fc95d4cb3e
amastronaut5 <137033143+amastronaut5@users.noreply.github.com>
2025-06-24 23:38:54 +0530

    register form

--- forms.py
+++ forms.py
@@ -14,14 +14,44 @@ class CreatePostForm(FlaskForm):
 
 
 # Create a form to register new users
+# class RegisterForm(FlaskForm):
+#     email = StringField("Email", validators=[DataRequired(), Email(message="Invalid email address. Please enter a valid email.")])
+#     password = PasswordField("Password", validators=[DataRequired(),Length(min=8, message="Password must be at least 8 characters."),
+#         Regexp(
+#             regex=r'^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$',
+#             message="Password must contain at least one uppercase letter, one number, and one special character."
+#         )])
+#     name = StringField("Name", validators=[DataRequired()])
+#     submit = SubmitField("Sign Me Up!")
+
+
 class RegisterForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email(message="Invalid email address. Please enter a valid email.")])
-    password = PasswordField("Password", validators=[DataRequired(),Length(min=8, message="Password must be at least 8 characters."),
-        Regexp(
-            regex=r'^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$',
-            message="Password must contain at least one uppercase letter, one number, and one special character."
-        )])
-    name = StringField("Name", validators=[DataRequired()])
+    email = EmailField("Email",
+                       validators=[
+            DataRequired(message="Email is required."),
+            Email(message="Invalid email address. Please enter a valid email.")])
+    name = StringField(
+        "Name",
+        validators=[DataRequired(message="Name is required.")])
+    password = PasswordField(
+        "Password",
+        validators=[
+            DataRequired(message="Password is required."),
+            Length(min=8, message="Password must be at least 8 characters."),
+            Regexp(
+                regex=r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$',
+                message="Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+            )
+        ]
+    )
+    confirm_password = PasswordField(
+        "Confirm Password",
+        validators=[
+            DataRequired(message="Please confirm your password."),
+            EqualTo('password', message="Passwords must match.")
+        ]
+    )
+    
     submit = SubmitField("Sign Me Up!")
 
 
